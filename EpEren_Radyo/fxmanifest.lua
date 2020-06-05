c70a49067a6cd063567fc526c2ca8e0fda54eb92
Eren <kurteren07@gmail.com>
2020-06-05 18:16:14 +0300

    v1

---
+++ EpEren_Radyo/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
+description 'EpEren Radyo ig/@ep.eren'
+
+version '1'
+
+
+client_scripts {"client/main.lua"}
+
+
+ui_page "HTML/index.html"
+
+files {
+    "HTML/index.html",
+	"HTML/*",
+    "HTML/assets/**/*",
+    "HTML/assets/img/radios/*"
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Fivem Radyo | Fivem Araba Radyosu
+
+## Setup
+> Insert your config "start EpEren_Radyo"
+---
+
+Geliştirci: &copy; [ErenKrt](https://www.instagram.com/ep.eren/)
